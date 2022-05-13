62eac030f0f4d642ce5cdcc71ebc52efd46ebc6c
JoshClopton <joshclopton@gmail.com>
2022-05-12 18:13:54 -0700

    add header and style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,1 +1,19 @@
-//this is a test for merge conflicts
+import React from "react";
+import siteLogo from "../../assets/images/logo-contributor.png";
+import "./Header.scss";
+import { Nav } from "../Nav/Nav";
+
+export const Header = () => {
+	return (
+		<header className="header">
+			<div className="header__container">
+				<a className="header__image" href="/" />
+				<div className="header__buttons">
+					<button className="header__user">Jacky</button>
+					<button className="header__language">English</button>
+				</div>
+			</div>
+			<Nav />
+		</header>
+	);
+};
