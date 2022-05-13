00b42925b7ff0b95b06916489ad524cbfae33c65
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-05-13 04:23:20 -0400

    Basic home page for user dashboard

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -4,6 +4,9 @@ import UserStats from '../../components/UserStats/UserStats';
 import CreativeCloud from '../../assets/images/CreativeCloud.jpg';
 import ProfileCard from '../../components/ProfileCard/ProfileCard';
 import Earnings from '../../components/Earnings/Earnings';
+import map from '../../assets/images/map.png';
+import bridge from '../../assets/images/BrooklynBridge.png';
+import brooklyn from '../../assets/images/Brooklyn.jpg';
 
 function HomePage() {
     return (
@@ -16,8 +19,31 @@ function HomePage() {
             <div className='UserPage-Right'>
                 <ProfileCard />
                 <Earnings />
-                <article>
-
+                <article className='downloads'>
+                    <div className='downloads-left'>
+                        <h4 className='downloads-left__title'>Latest Downloads</h4>
+                        <img className='downloads-left__map' src={map}></img>
+                    </div>
+                    <div className='downloads-right'>
+                        <div className='downloads-right__headers'>
+                            <p className='downloads-right__header'></p>
+                            <p className='downloads-right__header'>Location</p>
+                            <p className='downloads-right__header'>Comments</p>
+                            <p className='downloads-right__header'>Earnings</p>
+                        </div>
+                        <div className='downloads-right__rows'>
+                            <img className='downloads-right__img' src={bridge}></img>
+                            <p className='downloads-right__row'>Brooklyn, USA</p>
+                            <p className='downloads-right__row'>1800</p>
+                            <p className='downloads-right__row'>$19.75</p>
+                        </div>
+                        <div className='downloads-right__rows'>
+                            <img className='downloads-right__img' src={brooklyn}></img>
+                            <p className='downloads-right__row'>Brooklyn, USA</p>
+                            <p className='downloads-right__row'>650</p>
+                            <p className='downloads-right__row'>$19.75</p>
+                        </div>
+                    </div>
                 </article>
             </div>
         </main>
